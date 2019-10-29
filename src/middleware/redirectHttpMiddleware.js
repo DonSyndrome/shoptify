00ea4800fb571fe92a5acd227e9a58333b53bda2
shahar ram <shaharRam96@gmail.com>
2019-10-29 05:46:46 +0200

    fml

--- src/middleware/redirectHttpMiddleware.js
+++ src/middleware/redirectHttpMiddleware.js
@@ -4,7 +4,7 @@ exports.default = function getRedirectHttpMiddleware(options = {}) {
   // eslint-disable-next-line consistent-return
   return function redirectHttpMiddleware(req, res, next) {
     const secure = req.connection.encrypted || (req.get('X-Forwarded-Proto') === 'https');
-    if (secure && !dev) {
+    if (!secure && !dev) {
       const parts = req.get('host').split(':');
       const host = parts[0] || '127.0.0.1';
       return res.redirect(`https://${host}:${httpsPort}${req.url}`);
