e1de6e7921a62b473cacbd6571a69bb3a1213944
shahar ram <shaharRam96@gmail.com>
2019-10-29 05:07:27 +0200

    fix the bug?

--- src/middleware/redirectHttpMiddleware.js
+++ src/middleware/redirectHttpMiddleware.js
@@ -3,7 +3,8 @@ exports.default = function getRedirectHttpMiddleware(options = {}) {
   const dev = options.dev || false;
   // eslint-disable-next-line consistent-return
   return function redirectHttpMiddleware(req, res, next) {
-    if (req.protocol !== 'https' && !dev) {
+    const secure = req.connection.encrypted || (req.get('X-Forwarded-Proto') === 'https');
+    if (secure && !dev) {
       const parts = req.get('host').split(':');
       const host = parts[0] || '127.0.0.1';
       return res.redirect(`https://${host}:${httpsPort}${req.url}`);
