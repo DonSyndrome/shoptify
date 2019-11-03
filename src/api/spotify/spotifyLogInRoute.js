cedd449a59feb212a396e447180a59cda931a630
DonSyndrome <52895643+DonSyndrome@users.noreply.github.com>
2019-11-03 21:06:25 +0200

    add cockie domain (#21)

--- src/api/spotify/spotifyLogInRoute.js
+++ src/api/spotify/spotifyLogInRoute.js
@@ -5,7 +5,12 @@ const generateRandomString = require('../../utils/generateRandomString');
 const spotifyLogInRoute = (req, res) => {
   console.log('makeShure the login-with-spotify route work');
   const state = generateRandomString(16);
-  res.cookie(constants.SPOTIFY_STATE_KEY, state);
+  const cokiesOptions = { expires: new Date(Date.now() + 5000) };
+  if (process.env.DOMAIN !== 'localhost') {
+    cokiesOptions.domain = process.env.DOMAIN;
+    // cokiesOptions.secure = true;
+  }
+  res.cookie(constants.SPOTIFY_STATE_KEY, state, cokiesOptions);// 5 secondsz
   let scope = 'user-read-private user-read-email ';
   const { query } = res.req;
   if (query) {
@@ -13,12 +18,12 @@ const spotifyLogInRoute = (req, res) => {
       // parse becose the folow playlist is an array eg:
       // example.com?folow-playlist=['firstPlaylistId','secontPlaylistId']
       const playlistsToFolow = JSON.parse(query['folow-playlist']);
-      res.cookie(constants.PLAYLISTS_TO_FOLOW, playlistsToFolow);
+      res.cookie(constants.PLAYLISTS_TO_FOLOW, playlistsToFolow, cokiesOptions);
       scope += 'playlist-modify-private playlist-modify-public';
     }
     if (query.redirect) {
       const redirectURL = query.redirect;
-      res.cookie('redirect', redirectURL);
+      res.cookie('redirect', redirectURL, cokiesOptions);
     }
   }
   // your application requests authorization
